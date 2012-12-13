c5bb0ff35eaf7f5b2728d2a09a5b597c82472051
Eric Fahsl <efahsl@gmail.com>
2012-12-12 23:20:12 -0800

    fixed implementation

--- php/lib/analyticscode.php
+++ php/lib/analyticscode.php
@@ -2,13 +2,13 @@
 
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-35552544-1']);
-  _gaq.push(['_trackPageview']);
   <?php 
   	if(isset($_GET['resort'])) {
   		$resortName = $_GET['resort'];
   		echo "_gaq.push(['_setCustomVar',1,'resort','$resortName',3]);";
   	} ?>
-
+  _gaq.push(['_trackPageview']);
+  
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
