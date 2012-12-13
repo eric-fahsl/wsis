2b707b37f6782533b1866a62bf9f42ae6b3e7663
Eric Fahsl <efahsl@gmail.com>
2012-12-12 22:55:57 -0800

    added custom variable for resort tracking

--- php/lib/analyticscode.php
+++ php/lib/analyticscode.php
@@ -3,6 +3,11 @@
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-35552544-1']);
   _gaq.push(['_trackPageview']);
+  <?php 
+  	if(isset($_GET['resort'])) {
+  		$resortName = $_GET['resort'];
+  		echo "_gaq.push(['_setCustomVar',1,'resort','resortName']);"
+  	}
 
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
