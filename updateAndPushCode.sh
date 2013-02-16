a0a38958b0ddb86817970d47b8d9170a16e5cd4d
Ubuntu <ubuntu@ip-10-244-18-11.us-west-2.compute.internal>
2013-02-15 17:10:15 -0800

    added recursive to js folder

--- updateAndPushCode.sh
+++ updateAndPushCode.sh
@@ -4,5 +4,5 @@ git pull
 cp python/ski_resorts_snowfall.txt .
 sudo cp php/lib/* /var/www/lib
 sudo cp php/*.css /var/www
-sudo cp php/js/* /var/www/js
+sudo cp -r php/js/* /var/www/js
 sudo cp php/images/* /var/www/images
