ba69b17170004e5acc9ff86d4875620562a326d8
Ubuntu <ubuntu@ip-10-244-15-249.us-west-2.compute.internal>
2013-10-30 21:01:01 -0700

    added html push support

--- updateAndPushCode.sh
+++ updateAndPushCode.sh
@@ -4,5 +4,6 @@ git pull
 cp python/ski_resorts_snowfall.txt .
 sudo cp php/lib/* /var/www/lib
 sudo cp php/*.css /var/www
+sudo cp php/.html /var/www
 sudo cp -r php/js/* /var/www/js
 sudo cp php/images/* /var/www/images
