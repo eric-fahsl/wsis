f37bc86ceca1be059a18e8dc9b6b67e3049a70f6
Eric Fahsl <efahsl@gmail.com>
2013-10-31 23:02:20 -0700

    added jquery/google mapping library to just the maps page

--- php/lib/resortDetail.php
+++ php/lib/resortDetail.php
@@ -328,7 +328,9 @@ if (isset($_GET['resort'])) {
 	</div>
 
 
-
+    <script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+    <script src="js/jquery.ui.map.full.min.js" type="text/javascript"></script>
+    
 	<script type="text/javascript"> 
 		$('#map_canvas').gmap({ 'center': '41.8282,-100.5795' });
 		<?php
