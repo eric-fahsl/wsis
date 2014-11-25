c7d28f872f911f314997fde521ac504a9652b484
Eric Fahsl <efahsl@gmail.com>
2014-11-24 22:07:28 -0800

    creation for new deploy script

---
+++ deployApiAndSpa.sh
@@ -0,0 +1,11 @@
+echo "Deleting existing /var/www/ directory"
+sudo rm -r /var/www/*
+echo "DONE Deleting existing /var/www/ directory"
+
+echo "Copying PHP lib folder"
+sudo cp php/lib/* /var/www/lib
+echo "DONE Copying PHP lib folder"
+
+echo "Copying spa/dist folder to /var/www"
+sudo cp -r spa/dist/* /var/www
+echo "DONE Copying spa/dist folder to /var/www"
